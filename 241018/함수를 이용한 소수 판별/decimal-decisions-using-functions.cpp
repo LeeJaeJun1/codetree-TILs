438030083e350710f82a6f94d6252ad1e27ee0a2
LeeJaeJun1 <108278044+LeeJaeJun1@users.noreply.github.com>
2024-10-18 17:12:47 +0900

    Title: 함수를 이용한 소수 판별, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241018/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 241018/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 using namespace std;
 
-bool prime(int n) {
-    if(n==1) {  // n이 1일 때 소수 아니기 때문
-        return false;
-    }
-    for(int i = 2; i < n; i++) {
-        if(n%i == 0) { // 2부터 n까지 n을 나눴을 때 0이라는 것은 소수가 아니다.
-            return false;
+bool checkPrime(int n, int m) { // false일 경우 소수이다.
+    bool arr[1000] = {false, };
+    arr[0] = arr[1] = true;
+    int count{0};
+
+    for(int i = 2; i * i <= m; i++) {
+        if(arr[i]) continue; // true일 경우 소수 아니끼 때문에, continue
+
+        else{
+            for(int j = 2; j*i <= m; j++){
+                arr[j*i] = true;
+            }
         }
-    } 
+    }
     return true;
 }
 
 int main() {
-    int a{0};
-    int b{0};
-    int count{0};
+    int a,b;
     cin >> a >> b;
-    for(int j=a; j<=b; j++) {
-        if(prime(j)) {
-            count += j;
+    
+    int count{0};
+        if(!checkPrime(a,b)) {
+            count += ????;
         }
-    }
     cout << count;
     return 0;
 }
