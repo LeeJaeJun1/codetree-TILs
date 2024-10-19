b168b6e2e89480daf5eb1224b8ba7f4e62abe97f
LeeJaeJun1 <108278044+LeeJaeJun1@users.noreply.github.com>
2024-10-19 15:09:40 +0900

    Title: 두 수의 거듭제곱, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241019/두 수의 거듭제곱/two-numbers-of-squares.cpp
+++ 241019/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -2,10 +2,19 @@
 #include<cmath>
 using namespace std;
 
+int Power(int n, int m) {
+    int cnt = 1;
+    for(int i = 1; i <= m; i++) {
+        cnt *= n;
+    }
+    return cnt;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a,b;
+    int a;
+    int b;
     cin >> a >> b;
-    cout << pow(a,b);
+    cout << Power(a,b);
     return 0;
 }
