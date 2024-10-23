398ff7f3e10218d3e957d1822f14ff32c76d721a
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2024-10-23 16:36:33 +0900

    Title: 함수를 이용한 369 게임, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241023/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+// 3,6,9 중 하나가 들어가 있거나 or 그 숫자 자체가 3의 배수
+// num / 10 == 3,6,9 and num % 10 == 3,6,9 or num % 3 == 0
+
+bool Game(int num) {
+    if((num / 10 == 3 || num / 10 == 6 || num / 10 == 9 || num % 10 == 3 || num % 10 == 6 || num % 10 == 9)
+    || num%3==0) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    int a,b;
+    int count = 0;
+    cin >> a >> b;
+    for(int i = a; i <= b; i++) {
+        if(Game(i)) {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
