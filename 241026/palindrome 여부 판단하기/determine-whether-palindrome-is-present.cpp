97860c78368c4f7436576268e173eedbe15f026b
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2024-10-26 14:04:55 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
+++ 241026/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 bool isPalindrome(char *s, int len) {
     for(int i = 0; i < len/2; i++) {
-        if(s[i] == s[len-i-1]) {
-            return 1;
+        if(s[i] != s[len-i-1]) {
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 
 
