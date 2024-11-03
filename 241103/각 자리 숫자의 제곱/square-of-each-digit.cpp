e8757afabf905b61ebb9b1151b555b868fceaa2b
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2024-11-03 15:34:54 +0900

    Title: 각 자리 숫자의 제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241103/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n) {
+    if (n < 10) {
+        return n * n;
+    }
+    return sum(n /10) + (n % 10) * (n % 10);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << sum(n);
+    return 0;
+}
