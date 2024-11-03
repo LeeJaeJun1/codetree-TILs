18dd4bcb5265bdbf7a1e0a669dc4d781266aaa26
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2024-11-03 15:46:28 +0900

    Title: 1이 되는 순간까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241103/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+//
+
+int count(int n) {
+
+   if(n==1){
+       return 0;
+   }
+
+   if (n % 2 == 0) {
+       return count(n/2) + 1;
+   }
+   else{
+       return count(n/3) + 1;
+   }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << count(n);
+    return 0;
+}
