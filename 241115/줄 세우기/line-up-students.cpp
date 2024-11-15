1719aa05daa18ba7343a2405ca7ccbae4b731ab7
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2024-11-15 15:40:50 +0900

    Title: 줄 세우기, Time: 53ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241115/줄 세우기/line-up-students.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+class student {
+public:
+    int height;
+    int weight;
+    int number;
+    student(int h = 0, int w = 0, int n = 0) {
+        this->height = h;
+        this->weight = w;
+        this->number = n;
+    }
+};
+
+bool Cmp(student a, student b) {
+    if(a.height != b.height)
+        return a.height > b.height;
+    if(a.weight != b.weight)
+        return a.weight > b.height;
+    return a.number < b.number;
+}
+
+int main() {
+   int num;
+   cin >> num;
+   student s1[num];
+   int he, we;
+   for(int i = 0; i <num; i++) {
+       cin >> he >> we;
+       s1[i] = student(he,we,i+1);
+   }
+   sort(s1, s1+num, Cmp);
+
+   for(int j = 0; j < num; j++)
+       cout << s1[j].height << " " << s1[j].weight << " " << s1[j].number << endl;
+}
