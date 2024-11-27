884d2c10a5866c68d255d4d2bf3adae172282eec
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2024-11-27 16:10:44 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241127/Date to Date/date-to-date.cpp
+++ 241127/Date to Date/date-to-date.cpp
@@ -1,23 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int numofdays(int m, int n) {
+    int days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int total = 0;
+
+    for(int i = 1; i < m; i++){
+        total += days[i];
+    }
+    total += n;
+
+    return total;
+}
+
 int main() {
     int a,b,c,d;
     cin >> a >> b >> c >> d;
-    int month = a;
-    int total = 1;
-    int day = b;
-    int num_of_days[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-    while(true) {
-        if(month==c && day == d) {
-            break;
-        }
-        total++;
-        day++;
-        if(day > num_of_days[month]) {
-            month++;
-            day = 0;
-        }
-    }
-    cout << total;
+    cout << numofdays(c,d) - numofdays(a,b) + 1;
 }
