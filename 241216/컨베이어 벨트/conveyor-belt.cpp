0992aeb5a2d0ab6202850ff39d4a1d95f2309f4b
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2024-12-16 11:52:58 +0900

    Title: 컨베이어 벨트, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241216/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,t;
+    cin >> n >> t;
+    int arr1[100] = {}; int arr2[100] = {};
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+    for(int j = 0; j < n; j++) {
+        cin >> arr2[j];
+    }
+
+    while(t--) {
+        int temp1 = arr2[n-1];
+        int temp2 = arr1[n-1];
+
+        for(int a = n+1; a>=1; a--) {
+            arr1[a] = arr1[a-1];
+            arr2[a] = arr2[a-1];
+        }
+        arr1[0] = temp1;
+        arr2[0] = temp2;
+    }
+
+    for(int b = 0; b < n; b++) {
+        cout << arr1[b] << " ";
+    }
+    cout << endl;
+    for(int c = 0; c < n; c++) {
+        cout << arr2[c] << " ";
+    }
+}
